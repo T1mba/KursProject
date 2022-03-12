97b8da3623c8239f618acbe3ce5bffb62faee942
msergeev <makssergeev65983@gmail.com>
2022-03-12 10:23:17 +0300

    Сделал поиск и сортировку

--- KursIgimbaev/MainWindow.xaml.cs
+++ KursIgimbaev/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using KursIgimbaev.Models;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +21,61 @@ namespace KursIgimbaev
     /// <summary>
     /// Логика взаимодействия для MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : Window, INotifyPropertyChanged
     {
-        public IEnumerable<Product> ProductList { get; set; }
+        private IEnumerable<Product> _ProductList = null;
+        public IEnumerable<Product> ProductList
+        {
+            get
+            {
+               
+                
+
+                
+                var Result = _ProductList;
+                switch (SortType)
+                {
+                    case 1:
+                        Result = Result.OrderBy(p => p.FullName);
+                        break;
+
+                        case 2:
+                        Result = Result.OrderByDescending(p => p.FullName);
+                        break;
+                        
+                        case 3:
+                        Result = Result.OrderByDescending(p => p.Weight);
+                        break;
+                        case 4:
+                        Result = Result.OrderBy(p => p.Weight);
+                        break;
+                    case 5:
+                        Result = Result.OrderByDescending(p => p.Price);
+                        break;
+                    case 6:
+                        Result = Result.OrderBy(p => p.Price);
+                        break;
+                        
+                }
+                // ищем вхождение строки фильтра в названии и описании объекта без учета регистра
+                if (SearchFilter != "")
+                    Result = Result.Where(
+                        p => p.FullName.IndexOf(SearchFilter, StringComparison.OrdinalIgnoreCase) >= 0 ||
+            p.Category.IndexOf(SearchFilter, StringComparison.OrdinalIgnoreCase) >= 0
+
+
+                    );
+
+
+                return Result;
+            }
+            set
+            {
+                _ProductList = value;
+                Invalidate();
+            }
+        }
+
         public MainWindow()
         {
             InitializeComponent();
@@ -30,6 +83,38 @@ namespace KursIgimbaev
 
             Globals.DataProvider = new MySqlDataProvider();
             ProductList = Globals.DataProvider.GetProduct();
+        }
+
+        
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void Invalidate(string ComponentName = "ProductList")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(ComponentName));
+        }
+        private string SearchFilter = "";
+       
+
+        private void SearchFilterTextBox_KeyUp_1(object sender, KeyEventArgs e)
+        {
+            SearchFilter = SearchFilterTextBox.Text;
+            Invalidate();
+        }
+        public string[] SortList { get; set; } =
+        {
+            "Без сортировки",
+            "Название по убыванию",
+            "Название по возрастанию",
+            "Вес по убыванию",
+            "Вес по возростанию",
+            "Цена по убыванию",
+            "Цена по возрастанию" };
+        private int SortType = 0;
+        private void SortTypeComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            SortType = SortTypeComboBox.SelectedIndex;
+            Invalidate();
         }
     }
 }
